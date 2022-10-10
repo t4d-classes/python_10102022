de4afdfd038f5e820b091e9f4104e63094666da3
Eric Greene <eric@t4d.io>
2022-10-10 14:59:08 -0400

    add solution to exercise 2

--- demo-app/src/calc.py
+++ demo-app/src/calc.py
@@ -2,29 +2,61 @@
 
 result = 0
 
+history = []
+
+def get_next_id(history):
+
+  if (len(history) == 0):
+    return 1
+
+  ids = [ entry["id"] for entry in history ]
+  return max(ids) + 1
+
+
 command = input("Enter a command > ")
 
 while command:
 
   if command == "add":
     operand = int(input("Please enter an operand: "))
+    history.append({
+      "id": get_next_id(history), "command": "add", "operand": operand
+    })
     result = result + operand
     print(result)
   elif command == "subtract":
     operand = int(input("Please enter an operand: "))
+    history.append({
+      "id": get_next_id(history), "command": "subtract", "operand": operand
+    })
     result = result - operand
     print(result)
   elif command == "multiply":
     operand = int(input("Please enter an operand: "))
+    history.append({
+      "id": get_next_id(history), "command": "multiply", "operand": operand
+    })
     result = result * operand
     print(result)
   elif command == "divide":
     operand = int(input("Please enter an operand: "))
+    history.append({
+      "id": get_next_id(history), "command": "divide", "operand": operand
+    })
     result = result / operand
     print(result)
   elif command == "clear":
     result = 0
+    history = []
     print(result)
+  elif command == "history":
+    for entry in history:
+      print(entry)
+  elif command == "remove":
+    entryId = int(input("Please the id of the history entry to remove: "))
+    for entry in history:
+      if entry["id"] == entryId:
+        history.remove(entry)
   else:
     print("unknown command, please try again")
 
