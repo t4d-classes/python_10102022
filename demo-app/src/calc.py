45a7764612656907feec2063a23c3c8c62924587
Eric Greene <eric@t4d.io>
2022-10-10 16:42:03 -0400

    add exercise 4

--- demo-app/src/calc.py
+++ demo-app/src/calc.py
@@ -1,65 +1,79 @@
 
+def input_command():
+    return input("Enter a command > ")
 
-result = 0
+def input_operand():
+    return float(input("Please enter an operand: "))
 
-history = []
+def input_entry_id():
+    return int(input("Please the id of the history entry to remove: "))
 
-def get_next_id(history):
+def output_result(r):
+    print(f"Result: {r}")
 
-  if (len(history) == 0):
-    return 1
-
-  ids = [ entry["id"] for entry in history ]
-  return max(ids) + 1
-
-
-command = input("Enter a command > ")
-
-while command:
-
-  if command == "add":
-    operand = int(input("Please enter an operand: "))
-    history.append({
-      "id": get_next_id(history), "command": "add", "operand": operand
-    })
-    result = result + operand
-    print(result)
-  elif command == "subtract":
-    operand = int(input("Please enter an operand: "))
-    history.append({
-      "id": get_next_id(history), "command": "subtract", "operand": operand
-    })
-    result = result - operand
-    print(result)
-  elif command == "multiply":
-    operand = int(input("Please enter an operand: "))
-    history.append({
-      "id": get_next_id(history), "command": "multiply", "operand": operand
-    })
-    result = result * operand
-    print(result)
-  elif command == "divide":
-    operand = int(input("Please enter an operand: "))
-    history.append({
-      "id": get_next_id(history), "command": "divide", "operand": operand
-    })
-    result = result / operand
-    print(result)
-  elif command == "clear":
-    result = 0
-    history = []
-    print(result)
-  elif command == "history":
-    for entry in history:
-      print(entry)
-  elif command == "remove":
-    entryId = int(input("Please the id of the history entry to remove: "))
-    for entry in history:
-      if entry["id"] == entryId:
-        history.remove(entry)
-  else:
+def output_unknown_command():
     print("unknown command, please try again")
 
+def command_perform_math(result, history, command_name, math_op):
+    operand = input_operand()
+    history.append({
+        "id": get_next_id(history),
+        "command": command_name,
+        "operand": operand
+    })
+    result = math_op(result, operand)
+    output_result(result)
+
+    return (result, history)
+
+def command_print_history(history):
+    for entry in history:
+        print(entry)
+
+def command_remove_history_entry(history):
+    entry_id = input_entry_id()
+    for entry in history:
+        if entry["id"] == entry_id:
+              history.remove(entry)
+    return history
+
+def command_clear():
+    output_result(0)
+    return (0, [])
+
+def get_next_id(history):
+    if len(history) == 0:
+      return 1
+    ids = [ entry["id"] for entry in history ]
+    return max(ids) + 1
+
+def app():
+
+  result = 0
+  history = []
+
+  math_ops = {
+    "add": lambda a, b: a + b,
+    "subtract": lambda a, b: a - b,
+    "multiply": lambda a, b: a * b,
+    "divide": lambda a, b: a / b
+  }
+
+  command = input_command()
+
+  while command:
+      if (command in math_ops):
+          result, history = command_perform_math(result, history, command, math_ops[command])
+      elif command == "clear":
+          result, history = command_clear()
+      elif command == "history":
+          command_print_history(history)
+      elif command == "remove":
+          history = command_remove_history_entry(history)
+      else:
+          output_unknown_command()
+
+      command = input_command()
 
 
-  command = input("Enter a command > ")
+app()
---
+++ exercises/exercise-4.md
@@ -0,0 +1,9 @@
+# Exercise 4
+
+1. Remove the global result variable.
+
+2. Assuming the order of the history is the operation order.
+
+3. Write a function which calculates the result using the history. The function should have no print statements in it. Call the function from the appropriate place within the structure of your code.
+
+3. Ensure it works!
