a377a93c67b4e571139cf54bd41891000f5ceef8
Eric Greene <eric@t4d.io>
2022-10-12 15:49:38 -0400

    add exercise 8

---
+++ rates_app/dates_demo.py
@@ -0,0 +1,42 @@
+""" dates demo module """
+
+from datetime import datetime, timedelta, date
+import time
+
+
+# start_time = time.time()
+# time.sleep(1)
+# end_time = time.time()
+# print(end_time - start_time)
+
+# start_time = time.perf_counter()
+# time.sleep(1)
+# end_time = time.perf_counter()
+# print(end_time - start_time)
+
+# start = datetime.now()
+# print(start)
+
+# # end = start + timedelta(days=180)
+# end = timedelta(days=180) + start
+# print(end)
+
+# print(type(start))
+
+# print(type(timedelta(days=180)))
+
+# independence_day = date(1776, 7, 4)
+
+# print(independence_day)
+# print(type(independence_day))
+
+# print(datetime.now().strftime("%B %A"))
+
+# tax_day_str = "04-2022-18"
+
+# tax_day = datetime.strptime(tax_day_str, "%m-%Y-%d")
+
+# print(tax_day)
+# print(type(tax_day))
+
+# print(datetime.now().weekday())
